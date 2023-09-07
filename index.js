60139f15e2941bfa2048d72d4497121d364516ab
jhani2001 <jhani2001.alarcon@gmail.com>
2023-09-07 17:20:38 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res) => {
+    res.sendFile('./static/index.html', {
+        root: __dirname
+    }) 
+})
+
+app.use((req,res) => {
+    res.status(404).send('No se encontro tu pagina...!!!')
+})
+
+app.listen(3000)
+console.log(`Server on port ${3000}`)
